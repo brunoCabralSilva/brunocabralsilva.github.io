551d633c5a11df6ea7a2f83865912902ab25ffcd
Bruno Cabral <bruno.cabral.silva2018@gmail.com>
2022-08-20 15:34:35 -0300

    Componentização de items de tecnologias

---
+++ src/components/TechItem.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { DiJavascript, DiNodejsSmall } from "react-icons/di";
+import { FaDocker } from "react-icons/fa";
+import { SiRedux, SiTailwindcss } from "react-icons/si";
+import { GrMysql, GrReactjs } from "react-icons/gr";
+import { AiFillHtml5 } from 'react-icons/ai';
+import { IoLogoCss3 } from 'react-icons/io';
+
+export default function TechItem(props) {
+  const { text } = props;
+  const returnIcon = () => {
+    switch (text) {
+      case 'HTML':
+        return <AiFillHtml5 className="text-7xl mx-3 sm:mx-0 sm:mr-3 mr-0" />
+      case 'CSS':
+        return <IoLogoCss3 className="text-7xl mx-3" />
+      case 'JS':
+        return <DiJavascript className="text-7xl mx-3" />
+      case 'React':
+        return <GrReactjs className="text-7xl mx-3 mb-2 md:mx-3" />
+      case 'Redux':
+        return <SiRedux className="text-7xl mx-3 md:mx-3 mb-2" />
+      case 'Motion':
+        return (
+          <img 
+            src={require("../images/framer1.png")}
+            alt="ícone do framer motion"
+            className="object-contain w-28 px-4 pb-2 pt-1"
+          />);
+      case 'Tailwind':
+        return <SiTailwindcss className="text-7xl mx-3 md:mx-3 mb-2" />
+      case 'Docker': 
+        return <FaDocker className="text-7xl mx-3 md:mx-3 mb-2" />
+      case 'MySql':
+        return <GrMysql className="text-7xl mx-3 md:mx-3 mb-2" />
+      case 'Node':
+        return <DiNodejsSmall className="text-7xl mx-3 md:mx-3 mb-2" />
+      default:
+        return null;
+    }
+  }
+  return(
+    <div className="flex flex-col justify-center items-center">
+      { returnIcon() }
+      { text }
+    </div>
+  );
+}
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,62 +1,19 @@
 import React from 'react';
-import { DiJavascript, DiNodejsSmall } from "react-icons/di";
-import { FaDocker } from "react-icons/fa";
-import { SiRedux, SiTailwindcss } from "react-icons/si";
-import { GrMysql, GrReactjs } from "react-icons/gr";
-import { AiFillHtml5 } from 'react-icons/ai';
-import { IoLogoCss3 } from 'react-icons/io';
+import TechItem from './TechItem';
 
 export default function Technologies() {
+  const arrayTechnologies = ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Motion', 'Tailwind', 'Docker', 'MySql', 'Node'];
     return (
       <section className="w-10/12 my-20">
-        <p className="mb-2 md:text-left text-c sm:pb-3 pb-10 font-aboreto text-4xl sm:text-5xl md:text-6xl z-30">Tecnologias</p>
+        <p className="mb-2 md:text-left text-c pb-10 font-aboreto text-4xl sm:text-5xl md:text-6xl z-30">Tecnologias</p>
         <div className="flex flex-col md:flex-row flex-wrap w-full justify-center items-center flex-wrap items-center justify-center md:justify-start">
-          <div className="flex justify-center md:justify-end mt:0">
-            <div className="flex flex-col justify-center items-center">
-              <AiFillHtml5 className="text-7xl mr-3" />
-              HTML
-            </div>
-            <div className="flex flex-col justify-center items-center">
-            <IoLogoCss3 className="text-7xl mx-3" />
-              CSS
-            </div>
-            <div className="flex flex-col justify-center items-center">
-              <DiJavascript className="text-7xl mx-3" />
-              JS
-            </div>
-          </div>
-          <div className="flex flex-wrap justify-center md:justify-start ">
-            <div className="flex flex-col justify-center items-center mb-4 sm:mb-0 pt-6">
-            <GrReactjs className="text-7xl mx-3 mb-2 md:mx-3" />
-              React
-            </div>
-            <div className="flex flex-col justify-center items-center mb-4 sm:mb-0 pt-6">
-            <SiRedux className="text-7xl mx-3 md:mx-3 mb-2" />
-              Redux
-            </div>
-            <div className="flex flex-col justify-center items-center w-28">
-              <img 
-                src={require("../images/framer1.png")}
-                alt="ícone do framer motion"
-                className="object-contain px-4 pb-2 pt-1"
-              />
-              Motion
-            </div>
-            <div className="flex flex-col justify-center items-center">
-            <SiTailwindcss className="text-7xl mx-3 md:mx-3 mb-2" />
-              Tailwind
-            </div>
-            <div className="flex flex-col justify-center items-center">
-            <FaDocker className="text-7xl mx-3 md:mx-3 mb-2" />
-              Docker
-            </div>
-            <div className="flex flex-col justify-center items-center">
-            <GrMysql className="text-7xl mx-3 md:mx-3 mb-2" />
-              MySql
-            </div>
-            <div className="flex flex-col justify-center items-center">
-            <DiNodejsSmall className="text-7xl mx-3 md:mx-3 mb-2" />
-              Node
+          <div className="flex justify-center md:justify-end">
+            <div className="flex flex-wrap sm:flex-row justify-center sm:justify-start items-center">
+              {
+                arrayTechnologies.map((tec) => (
+                  <TechItem text={tec} />
+                ))
+              }
             </div>
           </div>
         </div>
