3e708a1ef5eb85e809f70043fc280780bdd35906
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2023-01-18 10:38:05 -0300

    Dinamização da página de artigos

--- src/pages/Articles.tsx
+++ src/pages/Articles.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Nav from "../components/Nav";
 import Header from "../components/Header";
+import articlesData from '../data/articles.json';
 
 interface ArticlesProps {
   history: any,
@@ -23,39 +24,21 @@ export default class Articles extends React.Component<ArticlesProps, {}> {
           </p>
         </section>
         <div className="my-10 grid grid-cols-1 sm:grid-cols-2 gap-1">
-          <a
-            href="https://community.revelo.com/como-conectar-o-node-js-com-o-mysql/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              src={require('../images/works-academy/post-001.png')}
-              alt="Conectando node.js e Mysql"
-              className="border-2 border-white hover:border-black"
-            />
-          </a>
-          <a
-            href="https://community.revelo.com/tres-passos-para-publicar-projetos-react-no-github/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              src={require('../images/works-academy/post-002.png')}
-              alt="Publicando projetos no github pages"
-              className="border-2 border-white hover:border-black"
-            />
-          </a>
-          <a
-            href="https://community.revelo.com/como-utilizar-redux-no-react/"
-            target="_blank"
-            rel="noreferrer"
-          >
-          <img
-            src={require('../images/works-academy/post-003.png')}
-            alt="usando redux no react"
-            className="border-2 border-white hover:border-black"
-          />
-          </a>
+          {
+            articlesData.map((article) => (
+              <a
+                href={ article.link }
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src={require(`../images/works-academy/revelo/${article.image}`)}
+                  alt={article.titulo }
+                  className="border-2 border-white hover:border-black"
+                />
+              </a>
+            ))
+          }
         </div>
       </div>
     );
